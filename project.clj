9f8a131913bfae47c174813496a4d658beb8f070
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-08-28 18:18:28 +0200

    Version 0.11.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/mint-storage "0.11.1-SNAPSHOT"
+(defproject org.zalando.stups/mint-storage "0.11.1"
   :description "The secret rotator and distributor."
   :url "https://github.com/zalando-stups/mint"
 
