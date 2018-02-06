2406fa3d1cd11646f27f7a0b8dc45eed97ae7d35
Torsten Wunderlich <torsten.wunderlich@zalando.de>
2018-02-06 15:02:10 +0100

    #38 do not delete application if no s3 bucket is specified

--- test/org/zalando/stups/mint/test_utils.clj
+++ test/org/zalando/stups/mint/test_utils.clj
@@ -1,5 +1,9 @@
 (ns org.zalando.stups.mint.test-utils
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [org.zalando.stups.friboo.config :as config]
+            [org.zalando.stups.mint.storage.sql :as sql]
+            [com.stuartsierra.component :as component]))
+
 
 (defn track
   "Adds a tuple on call for an action."
@@ -8,6 +12,16 @@
      (swap! a conj {:key  action
                     :args (into [] all-args)}))))
 
+(defmacro with-db [[db] & body]
+  `(let [config# (config/load-configuration
+                   (system/default-http-namespaces-and :db)
+                   [sql/default-db-configuration])
+         ~db (component/start (sql/map->DB {:configuration (:db config#)}))]
+     (try
+       ~@body
+       (finally
+         (component/stop ~db)))))
+
 (defmacro same!
   [x y]
   `(is (= ~x ~y)))
@@ -15,3 +29,7 @@
 (defmacro true!
   [x]
   `(same! true ~x))
+
+(defmacro instance!
+  [x y]
+  `(is (instance? ~x ~y)))
